48b1e6ec53a79dfa77dbb2217644cf58e736d22b
s1310247 <242957197+s1310247@users.noreply.github.com>
2025-12-19 15:37:01 +0900

    Revert "選択ソートのジェネレーター関数を実装し、メインスクリプトを追加"
    This reverts commit a5d5a29d6b48278fb8a3cfde23ca09ebfc0535bd.

--- src/SelectionSort.js
+++ src/SelectionSort.js
@@ -2,16 +2,15 @@ export class SelectionSort {
     /**
      * 配列をソートする関数
      * @param {number[]} array - ソート対象の数値配列 (例: [3, 1, 4])
-     * @yields {Generator} ソートされた配列
+     * @returns {number[]} ソートされた配列
      */
-    *sort(array) {
+    sort(array) {
         let n = array.length;
 
         for (let i = 0; i < n; i++) {
             let minIndex = i;
 
             for (let j = i + 1; j < n; j++) {
-                yield { type: 'compare', indices: [j, minIndex] };
                 if (array[j] < array[minIndex]) {
                     minIndex = j;
                 }
@@ -19,9 +18,8 @@ export class SelectionSort {
 
             if (minIndex !== i) {
                 [array[i], array[minIndex]] = [array[minIndex], array[i]];
-                yield { type: 'swap', indices: [i, minIndex] }
             }
         }
-        yield { type: 'done' }
+        return array;
     }
 }
