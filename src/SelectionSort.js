c1ea3be361c6a7b46d113207abdb485e2db9a7b6
s1310247 <242957197+s1310247@users.noreply.github.com>
2025-12-19 16:48:12 +0900

    format

--- src/SelectionSort.js
+++ src/SelectionSort.js
@@ -1,25 +1,25 @@
 export class SelectionSort {
-    /**
-     * 配列をソートする関数
-     * @param {number[]} array - ソート対象の数値配列 (例: [3, 1, 4])
-     * @returns {number[]} ソートされた配列
-     */
-    sort(array) {
-        let n = array.length;
+  /**
+   * 配列をソートする関数
+   * @param {number[]} array - ソート対象の数値配列 (例: [3, 1, 4])
+   * @returns {number[]} ソートされた配列
+   */
+  sort(array) {
+    let n = array.length;
 
-        for (let i = 0; i < n; i++) {
-            let minIndex = i;
+    for (let i = 0; i < n; i++) {
+      let minIndex = i;
 
-            for (let j = i + 1; j < n; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
-                }
-            }
-
-            if (minIndex !== i) {
-                [array[i], array[minIndex]] = [array[minIndex], array[i]];
-            }
+      for (let j = i + 1; j < n; j++) {
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
         }
-        return array;
+      }
+
+      if (minIndex !== i) {
+        [array[i], array[minIndex]] = [array[minIndex], array[i]];
+      }
     }
+    return array;
+  }
 }
--- src/TestEngine.js
+++ src/TestEngine.js
@@ -1,15 +1,15 @@
 export class TestEngine {
-    /**
-     * 配列が並んでいるかチェックし真偽値を返す
-     * @param {number[]} array 
-     * @returns {boolean}
-     */
-    static isSorted(array) {
-        for (let i = 0; i < array.length - 1; i++) {
-            if (array[i] > array[i + 1]) {
-                return false;
-            }
-        }
-        return true;
+  /**
+   * 配列が並んでいるかチェックし真偽値を返す
+   * @param {number[]} array
+   * @returns {boolean}
+   */
+  static isSorted(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        return false;
+      }
     }
+    return true;
+  }
 }
