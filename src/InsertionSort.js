8266d787dc31330e44fd84ebb5c62f5e6e28e0ae
s1290218 <s1290218@u-aizu.ac.jp>
2026-01-15 15:35:28 +0900

    sort関数をジェネレータ関数に変更

--- src/InsertionSort.js
+++ src/InsertionSort.js
@@ -2,23 +2,28 @@ export class InsertionSort {
   /**
    * 挿入ソートを用いて配列を昇順に並べ替える
    * @param {number[]} array ソート対象の1次元整数配列
-   * @returns {number[]} ソートされた配列
+   * @returns {Generator<{operation: string, indices: number[]}, void, undefined>} ソートの各ステップを表すオブジェクトを生成するジェネレーター
    */
-  sort(array) {
+  static *sort(array) {
     const n = array.length;
 
     for (let i = 1; i < n; i++) {
-      let j = i - 1;
-      const currentValue = array[i];
+      let j = i;
 
-      while (j >= 0 && array[j] > currentValue) {
-        array[j + 1] = array[j];
-        j--;
+      while (j > 0) {
+        yield { operation: 'compare', indices: [j - 1, j] };
+
+        if (array[j - 1] > array[j]) {
+          yield { operation: 'swap', indices: [j - 1, j] };
+
+          [array[j - 1], array[j]] = [array[j], array[j - 1]];
+
+          j--;
+        } else {
+          break;
+        }
       }
-
-      array[j + 1] = currentValue;
     }
-
-    return array;
   }
 }
+
